a43715c19c2d76649b4d65cfcc4d4ebfa93d13f4
Chamath Adithya <achamath1@gmail.com>
2025-10-30 23:58:10 +0530

    wrong

--- src/model/Payment.java
+++
@@ -1,5 +0,0 @@
-package model;
-
-public abstract class Payment {
-    public abstract void pay(double amount);
-}
--- src/module-info.java
+++
@@ -1,9 +0,0 @@
-module RetailStore {
-    requires javafx.controls;
-    requires javafx.fxml;
-    exports retailstore;
-    exports model;
-    exports payment;
-    exports service;
-    exports util;
-}
